af5d1c0dcb0763411cd9a61e80251e8034be84a7
Sajeev Ramasamy <thorion3006@gmail.com>
2017-02-19 19:18:51 +0530

    scope. added

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -43,9 +43,9 @@
                 scope.trackThumb = function() {
                     $document.bind('mousemove.thumb', function(event) {
                         var percent = calculatePercent(seekBar, event);
-                        
+                        scope.$apply(function() {
                             scope.value = percent * scope.max;
-                        
+                        });
                     });
 
                     $document.bind('mouseup.thumb', function() {
